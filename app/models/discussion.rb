0bfb5ccd7b28e96568abab56d56d697bcc9dc7c3
gabriel <gabriel@byel.dev>
2021-04-08 22:58:20 -0300

    Discussions index, turbo stream

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -1,6 +1,12 @@
 class Discussion < ApplicationRecord
   belongs_to :user, default: -> { Current.user }
 
+  after_create_commit -> { broadcast_prepend_to "discussions"}
+  after_update_commit -> { broadcast_replace_to "discussions"}
+  after_destroy_commit -> { broadcast_remove_to "discussions"}
   validates :name, presence: true
 
+  def to_param
+    "#{id}-#{name.downcase}".parameterize
+  end
 end
