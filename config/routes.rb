d788d7e7f8eed7ee25434a8599f34e9b9945e18a
gabriel <gabriel@byel.dev>
2021-04-24 16:03:36 -0300

    realtime delation of posts

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "main#index"
   resources :discussions do
-    resources :posts, only: [:create, :edit, :update, :show], module: :discussions
+    resources :posts, only: [:create, :edit, :update, :show, :destroy], module: :discussions
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   mount Attractor::Rails::Engine, at: "/attractor" if Rails.env.development?
